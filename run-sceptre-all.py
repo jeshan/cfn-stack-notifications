0cc290792cf288906b715b2c50f4a9f9a31ce0a1
jeshan <j@jeshan.co>
2019-04-20 00:38:40 +0400

    exit code with fail count

--- run-sceptre-all.py
+++ run-sceptre-all.py
@@ -1,13 +1,18 @@
+import sys
 from glob import glob
 from subprocess import check_output, CalledProcessError
 
+fail_count = 0
+
 
 def run(command):
+    global fail_count
     try:
         output = check_output(command.split(' ')).decode('utf-8')
         print(output)
     except CalledProcessError as exc:
         print("Status : FAIL while running", command, exc.returncode, exc.output.decode('utf-8'))
+        fail_count += 1
 
 
 def go():
@@ -19,3 +24,4 @@ def go():
 
 if __name__ == '__main__':
     go()
+    sys.exit(fail_count)
