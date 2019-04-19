906226097f6cea2441db9745d82e801e584d0031
jeshan <j@jeshan.co>
2019-04-19 22:00:55 +0400

    get all parameters by path

--- configure-aws-cli.py
+++ configure-aws-cli.py
@@ -7,24 +7,26 @@ import yaml
 
 
 def _configure_profile(profile_name, profile_role):
+    print('Got', profile_role, 'for', profile_name)
     check_output(f'aws configure set profile.{profile_name}.region us-east-1')
     check_output(f'aws configure set profile.{profile_name}.credential_source EcsContainer')
     check_output(f'aws configure set profile.{profile_name}.role_arn {profile_role}')
 
 
-def configure_profile(client, repo, profile_name):
+def configure_profile(repo, profile_name, parameters):
     name = f'/github.com/{repo}/aws-profile-roles/{profile_name}'
-    print(f'Getting parameter {name}')
-    role = client.get_parameter(Name=name)['Parameter']['Value']
-    _configure_profile(profile_name, role)
+    for param in parameters:
+        if param['Name'] == name:
+            _configure_profile(profile_name, param['Value'])
 
 
 def go(repo):
     client = boto3.client('ssm')
+    parameters = client.get_parameters_by_path(Path=f'/github.com/{repo}', Recursive=True)['Parameters']
     for path in glob('config/*/config.yaml'):
         profile_name = yaml.load(open(path))['profile']
-        configure_profile(client, repo, profile_name)
-    configure_profile(client, repo, 'default')
+        configure_profile(repo, profile_name, parameters)
+    configure_profile(repo, 'default', parameters)
 
 
 if __name__ == '__main__':
