b7bc85a29066e916c095838ba831bb6f3911dd38
jeshan <j@jeshan.co>
2019-04-20 20:01:01 +0400

    Revert "test delete permissions"
    This reverts commit bf468d39

--- run-sceptre-all.py
+++ run-sceptre-all.py
@@ -19,7 +19,7 @@ def go():
     for path in glob('config/*'):
         if '.' not in path:
             env = path[path.rindex('/') + 1:]
-            run(f'sceptre --no-colour delete -y {env}')
+            run(f'sceptre --no-colour launch -y {env}')
 
 
 if __name__ == '__main__':
