8a8652c03ec1c08bd2c1a5b141e90841b5947fca
jeshan <j@jeshan.co>
2019-04-20 20:55:44 +0400

    script to upload private config

---
+++ upload-private-config.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+BRANCH=`git rev-parse --abbrev-ref HEAD`
+echo "On branch ${BRANCH}"
+
+aws s3 sync --delete --exclude "*" --include "config/*" --exclude "config/config.yaml" \
+  --exclude "config/dev/*" --exclude "config/deployment/*" \
+  . s3://jeshan-oss-private-files/github.com/jeshan/cfn-stack-notifications/${BRANCH}/
