26a7788182ed267238a98bb69406e869029f1a46
jeshan <j@jeshan.co>
2019-04-20 15:01:35 +0400

    write target account config file

--- generate-config.py
+++ generate-config.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 regions = 'us-east-1,us-east-2,eu-west-1,us-west-1,us-west-2,ap-south-1,ap-southeast-1,ap-southeast-2,ca-central-1,eu-central-1,eu-north-1,eu-west-2,eu-west-3,sa-east-1,ap-northeast-1,ap-northeast-2'.split(
     ',')
@@ -6,6 +7,12 @@ regions = 'us-east-1,us-east-2,eu-west-1,us-west-1,us-west-2,ap-south-1,ap-south
 
 def go(env):
     open(f'config/{env}/config.yaml', 'w')
+    with open(f'config/{env}/base.yaml', 'w') as f:
+        f.write(f"""template_path: deployment-target-account.yaml
+
+parameters:
+  DeployerRole: arn:aws:iam::{os.environ['ACCOUNT_ID']}:role/service-role/cfn-stack-notifications-deployer
+""")
     for region in regions:
         with open(f'config/{env}/{region}.yaml', 'w') as f:
             f.write(f"""template_path: template.yaml
