75e68d03f6ad51d97a1c1fb56411ab45deab0174
jeshan <j@jeshan.co>
2019-04-20 19:15:27 +0400

    rm profile

--- upload-public-templates.sh
+++ upload-public-templates.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-aws s3 cp --profile jeshanco templates/template.yaml s3://jeshan-oss-public-files/cfn-stack-notifications-template.yaml
-aws s3 cp --profile jeshanco templates/deployment-pipeline.yaml s3://jeshan-oss-public-files/cfn-stack-notifications-deployment-pipeline-template.yaml
+aws s3 cp templates/template.yaml s3://jeshan-oss-public-files/cfn-stack-notifications-template.yaml
+aws s3 cp templates/deployment-pipeline.yaml s3://jeshan-oss-public-files/cfn-stack-notifications-deployment-pipeline-template.yaml
