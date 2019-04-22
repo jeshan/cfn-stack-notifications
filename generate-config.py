853ee4f0211e1aeea365a3e4fa184d601eae1a76
jeshan <j@jeshan.co>
2019-04-22 23:49:32 +0400

    update generator

--- generate-config.py
+++ generate-config.py
@@ -3,8 +3,6 @@ import sys
 from os import mkdir
 from os.path import exists
 
-from mako.template import Template
-
 regions = 'us-east-1,us-east-2,eu-west-1,us-west-1,us-west-2,ap-south-1,ap-southeast-1,ap-southeast-2,ca-central-1,eu-central-1,eu-north-1,eu-west-2,eu-west-3,sa-east-1,ap-northeast-1,ap-northeast-2'.split(
     ',')
 
@@ -14,13 +12,6 @@ def _mkdir(path):
         mkdir(path)
     except:
         pass
-
-
-def save_template_file(path, data, directory=None):
-    basename = path[(path.rindex('/') + 1) if '/' in path else 0:]
-    with open(path if not directory else f'{directory}/{basename}', 'w') as f:
-        src = path[:path.rindex('.') + 1] + 'template.' + path[path.rindex('.') + 1:]
-        f.write(Template(filename=src).render(**data))
 
 
 def go(env):
@@ -43,6 +34,9 @@ def go(env):
     with open('config/config.yaml', 'w') as f:
         f.write(f"""project_code: {project_name}
 region: us-east-1
+
+dlq_name: lambda-default-dlq
+events_topic_name: cloudformation-stack-events
 """)
 
     with open('config/app/deployment/pipeline.yaml', 'w') as f:
@@ -58,7 +52,7 @@ parameters:
         f.write(f"""template_path: deployment-target-account.yaml
 
 parameters:
-  DeploymentAccount: {account_id}
+  DeploymentAccount: '{account_id}'
   ProjectName: {{{{stack_group_config.project_code}}}}
 """)
     for region in regions:
