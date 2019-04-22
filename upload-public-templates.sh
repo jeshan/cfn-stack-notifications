4562fa65b0124920bef69bc045f5ebfbd5a91cca
jeshan <j@jeshan.co>
2019-04-22 13:12:36 +0400

    set -e

--- upload-public-templates.sh
+++ upload-public-templates.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 
 aws s3 cp templates/template.yaml s3://jeshan-oss-public-files/cfn-stack-notifications-template.yaml
 aws s3 cp templates/deployment-pipeline.yaml s3://jeshan-oss-public-files/cfn-stack-notifications-deployment-pipeline-template.yaml
