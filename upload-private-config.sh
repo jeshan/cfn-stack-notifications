31045ff7b69d3e9e67f37765f300bd3237156ac7
jeshan <j@jeshan.co>
2019-04-23 00:27:21 +0400

    fix sync script

--- upload-private-config.sh
+++ upload-private-config.sh
@@ -4,6 +4,4 @@ set -e
 BRANCH=`git rev-parse --abbrev-ref HEAD`
 echo "On branch ${BRANCH}"
 
-aws s3 sync --delete --exclude "*" --include "config/*" --exclude "config/config.yaml" \
-  --exclude "config/dev/*" --exclude "config/deployment/*" \
-  . s3://jeshan-oss-private-files/github.com/jeshan/cfn-stack-notifications/${BRANCH}/
+aws s3 sync --delete --exclude "*" --include "config/*" --exclude "config/config.yaml" --exclude "config/app/dev/*" --exclude "config/app/deployment/*" . s3://jeshan-oss-private-files/github.com/jeshan/cfn-stack-notifications/$BRANCH/
